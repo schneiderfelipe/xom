719c1a008aacbe38abba44de05f1e632cb4fe64a
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-07-13 09:07:11 -0300

    Create empty elements

--- src/xom.nim
+++ src/xom.nim
@@ -2,7 +2,10 @@ import macros, macroutils, xmltree, dom
 
 func createTree*(x: XmlNode): NimNode =
   if x.kind == xnElement:
-    discard
+    result = superQuote do:
+      document.createElement(`x.tag`)
   elif x.kind == xnText:
     result = superQuote do:
       document.createTextNode(`x.text`)
+  else:
+    raise newException(ValueError, "unsupported XML node type: " & $x.kind)
--- tests/tests.nim
+++ tests/tests.nim
@@ -21,3 +21,10 @@ suite "Basics":
     let x = document.body.appendChildAndReturn html"Hello, world!"
     check x.nodeName == "#text"
     check x.textContent == "Hello, world!"
+    check document.body.childNodes[2] == x
+
+  test "can create empty elements":
+    let x = document.body.appendChildAndReturn html"<h1></h1>"
+    check x.nodeName == "H1"
+    check x.textContent == ""
+    check document.body.childNodes[3] == x

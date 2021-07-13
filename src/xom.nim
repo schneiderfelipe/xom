b3bc537d04c2edfd59d4b8a479aef0e6d5a6bda2
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-07-13 14:18:48 -0300

    Review implementation

--- src/xom.nim
+++ src/xom.nim
@@ -1,46 +1,58 @@
-import dom, macros, macroutils, strtabs, strutils, tables, xmltree
+when not defined(js) and not defined(Nimdoc):
+  {.error: "This module only works on the JavaScript platform".}
+
+import dom, macros, macroutils, strformat, strtabs, strutils, tables, xmltree
 
 
 var id {.compileTime.}: CountTable[char]
 
 
-proc createIdentFor(x: XmlNode): NimNode =
+proc createIdentFor(x: XmlNode): auto {.compileTime.} =
   ## Create a cute unique identifier for the node in the form `"<char><id>"`,
   ## where "`<char>`" is the first letter of the tag name and "`<id>`" is a an
   ## increasing number starting at zero.
   let c = x.tag[0]
-  result = ident(c & $id[c])
+  result = ident(&"{c}{id[c]}")
   id.inc(c)
 
 
-proc createTree*(x: XmlNode): NimNode =
+func adjustText(s: string): string {.compileTime.} =
+  ## Remove whitespace as much as possible, hopefully not breaking anything.
+  ## Basically, contigous whitespace is transformed into single `' '`
+  ## characters.
+  for i in low(s)..high(s):
+    if not (s[i].isSpaceAscii and i > 0 and s[i-1].isSpaceAscii):
+      result &= s[i]
+
+
+proc createTree*(x: XmlNode): NimNode {.compileTime.} =
+  ## Create a `NimNode` that constructs an HTML element with the same
+  ## structure as `x` by using DOM calls. HTML comments are ignored and, if
+  ## the whole tree is ignored, a `NimNode` representing `nil` is returned.
   if x.kind == xnElement:
     result = superQuote do:
       document.createElement(`x.tag`)
+
     if len(x) > 0 or attrsLen(x) > 0: # or forceEntry(x):
       let n = createIdentFor(x)
-      result = newStmtList superQuote do:
+
+      result = newStmtList quote do:
         let `n` = `result`
 
       if not isNil(x.attrs):
-        for k, v in x.attrs:
-          result.add superQuote do:
-            `n`.setAttribute(`k`, `v`)
+        for key, value in x.attrs:
+          result.add newCall(ident"setAttribute", n, key, value)
 
-      for xc in x:
-        let nc = createTree(xc)
-        if nc.kind != nnkNilLit:
-          result.add superQuote do:
-            `n`.appendChild(`nc`)
+      for xchild in x:
+        let nchild = createTree(xchild)
+        if nchild.kind != nnkNilLit:
+          result.add newCall(ident"appendChild", n, nchild)
 
       result.add n
   elif x.kind == xnText:
-    var nt = x.text
-    if nt.isEmptyOrWhitespace:
-      nt = " "
     result = superQuote do:
-      document.createTextNode(`nt`)
+      document.createTextNode(`adjustText(x.text)`)
   elif x.kind == xnComment:
     discard
   else:
-    raise newException(ValueError, "unsupported XML node type: '" & $x.kind & "'")
+    raise newException(ValueError, &"unsupported XML node type: '{x.kind}'")

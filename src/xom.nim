292f44e5ecf4810f116658d5b4b0d910ea2eaefe
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-07-15 21:52:46 -0300

    Implement minor code improvements

--- src/xom.nim
+++ src/xom.nim
@@ -17,7 +17,7 @@ type
     ## An object holding context for a conversion between `XmlNode` and
     ## `NimNode`. It accepts some callbacks, which serve as transformers,
     ## since the given object can be modified prior to this generation.
-    tree: XmlNode ## The XML represented by the object.
+    node: XmlNode ## The XML represented by the object.
     id: CountTable[char] ## Keep track of how many elements of each type were already generated.
     buffer: string ## Buffer used during aglutination of text nodes.
     onEnter*: XmlNode -> Command
@@ -35,10 +35,10 @@ type
       ## variable name as a parameter.
 
 
-func initXom*(x: XmlNode): Xom {.compileTime.} =
+func initXom*(node: XmlNode): Xom {.compileTime.} =
   ## Initialize a `Xom` object with a `XmlNode`. Default behavior of all
   ## callbacks is to emit code to create nodes.
-  result = Xom(tree: x)
+  result = Xom(node: node)
   result.onEnter = func(node: XmlNode): Command =
     Emit
   result.onEmitNamed = func(node: XmlNode, name: NimNode) =
@@ -64,120 +64,120 @@ func adjustText(s: string): string {.compileTime.} =
       result &= ' '
 
 
-func flushBufferTo(stmts, n: NimNode, context: Xom) {.compileTime.} =
+func flushBufferTo(stmts, nnode: NimNode, context: Xom) {.compileTime.} =
   ## Flush the buffer of a `Xom` object by adding code to `result` to produce
-  ## a text node attached to `n` with the buffer contents. Do nothing if the
+  ## a text node attached to `nnode` with the buffer contents. Do nothing if the
   ## buffer is empty.
   if len(context.buffer) > 0:
     stmts.add superQuote do:
-      `n`.appendChild(document.createTextNode(`adjustText(context.buffer)`))
+      `nnode`.appendChild(document.createTextNode(`adjustText(context.buffer)`))
     context.buffer = ""
     assert len(context.buffer) == 0, "Buffer not empty after flush"
 
 
-func createIdentFor(x: XmlNode, context: Xom): NimNode {.compileTime.} =
+func createIdentFor(node: XmlNode, context: Xom): NimNode {.compileTime.} =
   ## Create a cute unique identifier for an XML node in the form
   ## `"<char><id>"`, where "`<char>`" is the first letter of the tag name and
   ## "`<id>`" is a an increasing number starting at zero.
-  let c = case x.kind:
+  let c = case node.kind:
   of xnElement:
-    x.tag[0]
+    node.tag[0]
   of xnText:
     't'
   else:
-    raise newException(ValueError, "unsupported XML node kind: " & $x.kind)
+    raise newException(ValueError, "unsupported XML node kind: " & $node.kind)
   result = ident(c & $context.id[c])
   context.id.inc(c)
 
 
-proc toNimNodeImpl(x: XmlNode, context: Xom,
+proc toNimNodeImpl(node: XmlNode, context: Xom,
     assigns: NimNode): NimNode {.compileTime.} =
   ## Create a `NimNode` that constructs an HTML element with the same
   ## structure as `x` by using DOM calls, and use the context of the `Xom`
   ## object `q`. HTML comments are ignored and, if the whole tree is ignored,
   ## a `NimNode` representing `nil` is returned.
-  case x.kind:
+  case node.kind:
   of xnElement:
-    let enterCommand = context.onEnter(x)
+    let enterCommand = context.onEnter(node)
     if enterCommand == Discard:
       return newNilLit()
 
     result = superQuote do:
-      document.createElement(`x.tag`)
+      document.createElement(`node.tag`)
 
-    if len(x) > 0 or attrsLen(x) > 0 or enterCommand == EmitNamed:
-      let n = createIdentFor(x, context)
+    if len(node) > 0 or attrsLen(node) > 0 or enterCommand == EmitNamed:
+      let nnode = createIdentFor(node, context)
       if enterCommand != EmitNamed:
         result = newStmtList quote do:
-          let `n` = `result`
+          let `nnode` = `result`
       else:
-        context.onEmitNamed(x, n)
+        context.onEmitNamed(node, nnode)
         assigns.add quote do:
-          let `n` = `result`
+          let `nnode` = `result`
         result = newStmtList()
 
-      if not isNil(x.attrs):
-        for key, value in x.attrs:
+      if not isNil(node.attrs):
+        for key, value in node.attrs:
           result.add quote do:
-            `n`.setAttribute(`key`, `value`)
+            `nnode`.setAttribute(`key`, `value`)
 
-      for xchild in x:
-        case xchild.kind:
+      for child in node:
+        case child.kind:
         of xnElement:
-          flushBufferTo(result, n, context)
-          let nchild = toNimNodeImpl(xchild, context, assigns)
+          flushBufferTo(result, nnode, context)
+          let nchild = toNimNodeImpl(child, context, assigns)
           if nchild.kind != nnkNilLit:
             result.add quote do:
-              `n`.appendChild(`nchild`)
+              `nnode`.appendChild(`nchild`)
         of xnText:
-          let enterCommand = context.onEnter(xchild)
+          let enterCommand = context.onEnter(child)
           if enterCommand == Discard:
             continue
 
           if enterCommand != EmitNamed:
-            context.buffer &= xchild.text
+            context.buffer &= child.text
           else:
-            flushBufferTo(result, n, context)
-            let t = createIdentFor(xchild, context)
-            context.onEmitNamed(xchild, t)
+            flushBufferTo(result, nnode, context)
+            let text = createIdentFor(child, context)
+            context.onEmitNamed(child, text)
             assigns.add superQuote do:
-              let `t` = document.createTextNode(`adjustText(xchild.text)`)
+              let `text` = document.createTextNode(`adjustText(child.text)`)
             result.add quote do:
-              `n`.appendChild(`t`)
+              `nnode`.appendChild(`text`)
         of xnComment:
           continue
         else:
-          raise newException(ValueError, "unsupported XML node kind: " & $xchild.kind)
+          raise newException(ValueError, "unsupported XML node kind: " & $child.kind)
 
-      flushBufferTo(result, n, context)
-      result.add n
+      flushBufferTo(result, nnode, context)
+      result.add nnode
   of xnText:
-    let enterCommand = context.onEnter(x)
+    let enterCommand = context.onEnter(node)
     if enterCommand == Discard:
       return newNilLit()
 
     result = superQuote do:
-      document.createTextNode(`adjustText(x.text)`)
+      document.createTextNode(`adjustText(node.text)`)
 
     if enterCommand == EmitNamed:
-      let n = createIdentFor(x, context)
-      context.onEmitNamed(x, n)
+      let nnode = createIdentFor(node, context)
+      context.onEmitNamed(node, nnode)
       assigns.add quote do:
-        let `n` = `result`
-      result = n
+        let `nnode` = `result`
+      result = nnode
   of xnComment:
     return newNilLit()
   else:
-    raise newException(ValueError, "unsupported XML node kind: " & $x.kind)
+    raise newException(ValueError, "unsupported XML node kind: " & $node.kind)
 
 
-converter toNimNode*(x: Xom): NimNode {.compileTime.} =
+converter toNimNode(node: Xom): NimNode {.compileTime.} =
   ## Convert a `Xom` object to a `NimNode`. This converter constructs an HTML
   ## element with the same structure as `x` by using DOM calls. HTML comments
   ## are ignored and, if the whole tree is ignored, a `NimNode` representing
   ## `nil` is returned.
   let assigns = newStmtList()
-  result = toNimNodeImpl(x.tree, x, assigns)
+  result = toNimNodeImpl(node.node, node, assigns)
   if len(assigns) > 0:
     for i, assign in assigns:
       result.insert i, assign
